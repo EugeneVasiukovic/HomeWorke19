3d72c3702e701afab2621d6e2dc5920cc6e3d7f3
yeuheni.vasiukovich@winfinity.live <yeuheni.vasiukovich@winfinity.live>
2025-02-23 01:33:41 +0300

    add waiter class and method

--- src/main/java/homework22java/OneTaskArrayForeach.java
+++ src/main/java/homework22java/OneTaskArrayForeach.java
@@ -5,7 +5,7 @@ public class OneTaskArrayForeach {
     public static void main(String[] args) {
         Integer[] numberOfDisplay = {5,6,7,8,9,10,11,12,13,14,15};
         for (int i : numberOfDisplay){
-            System.out.println("Massiv NnumberOfDisplay: " + i);
+            System.out.println("Array NnumberOfDisplay: " + i);
         }
     }
 }
